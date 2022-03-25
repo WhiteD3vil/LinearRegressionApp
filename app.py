fc9838c75ea75b8da521be0cf8edf42836fe311c
Deepak Ravikumar <deepak.r@techmango.net>
2022-03-25 23:51:40 +0530

    procfile changes

--- app.py
+++ app.py
@@ -17,12 +17,11 @@ def main():
     This function is used to display the app.
     """
     st.title("Admission Prediction App")
-    st.text("This app is used to predict the chance of admission based on the input parameters.")
     html_temp="""
-    <div>
-    <h2>Admission Prediction App</h2>
-    </div>
-    """
+        <div style="background-color:tomato;padding:10px">
+        <h2 style ="color:black;text-align:center;">Admission Prediction ML app</h2>
+        </div>
+        """
     st.markdown(html_temp,unsafe_allow_html=True)
     GREScore = st.text_input("GRE Score")
     TOEFLScore = st.text_input("TOEFL Score")
@@ -31,7 +30,7 @@ def main():
     result = ""
     if st.button("Predict"):
         result = predict_chance(GREScore,TOEFLScore,UniversityRanking,CGPA)
-        st.success(f"The chance of admission is {result}%")
+    st.success(f"The chance of admission is {result}%")
 
 if __name__ == "__main__":
     main()
